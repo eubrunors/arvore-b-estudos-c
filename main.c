bd507005f153a840de30f59a03c41428917ad00c
eubrunors <bruno.2013.br11@gmail.com>
2025-07-22 14:47:02 -0300

    feat: implementar a logica de split de uma raiz

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define ordem 4 // Maximum degree of the B-tree
+#define ORDEM 4 // Maximum degree of the B-tree
 
 struct BTreeNo {
     int qtd_chaves; // Number of keys currently in the node
-    int chaves[ordem-1]; // Array of keys
-    struct BTreeNo *filho[ordem]; // Array of child pointers
+    int chaves[ORDEM-1]; // Array of keys
+    struct BTreeNo *filho[ORDEM]; // Array of child pointers
     bool eh_folha; // True if node is a leaf
 };
 // Function to create a new node
@@ -15,21 +15,58 @@ struct BTreeNo *criaNo(bool eh_folha)
 {
     struct BTreeNo *novoNo = (struct BTreeNo *)malloc(sizeof(struct BTreeNo));
     novoNo->qtd_chaves = 0;
-    novoNo-> = eh_folha;
-    for (int i = 0; i < M; i++) {
+    novoNo->eh_folha = eh_folha;
+    for (int i = 0; i < ORDEM; i++) {
         novoNo->filho[i] = NULL;
     }
     return novoNo;
 }
 void insere(**raiz, chave){
     struct BTreeNo *no = *raiz
+    //INSERINDO EM NO RAIZ
     if(no==NULL){
         *raiz = criaNo(true);
         (*raiz)->chaves[0] = chave;
         (*raiz)->qtd_chaves = 1;
+    }else{
+        //SPLIT DE RAIZ
+        if(no->qtd_chaves==ORDEM-1){
+            struct BTreeNo *novaRaiz = criaNo(false);
+            novaRaiz->filho[0] = no;
+            splitFilho(novaRaiz,0);
+            *raiz = novaRaiz;
+        }
+        //INSERIR EM NO NAO LOTADO
+        insereNaoCheio(*raiz,cahve);
     }
 }
+void splitFilho(struct BTreeNo *pai,int indice){
+    struct BTreeNo *filho = pai->filho[indice];
+    struct BTreeNo *novoNo = criaNo(filho->eh_folha);
 
+    int meio=ORDEM/2;
+
+    novoNo->qtd_chaves = ORDEM/2-1;//1 CHAVE PRO NOVATO
+
+    for(int i=0, i<ORDEM/2-1; i++){
+        novoNo->chaves[i]=filho->chaves[i+ORDEM/2];
+    }
+    if(!filho->is_leaf){
+        //SE FILHO NAO FOR FOLHA
+        for (int j = 0; j < ORDEM / 2; j++) {
+            novoNo->filho[j] = filho->filho[j + meio];
+        }
+    }
+    // Reduz a quantidade de chaves do nó original
+    filho->qtd_chaves=ORDEM/2-1;
+
+
+    pai->filho[indice+1]=novoNo;
+
+
+    pai->chaves[indice]=filho->chaves[meio-1];
+
+    pai->qtd_chaves++;
 }
 
 int main()
